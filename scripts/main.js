90d595896a500d60d3f8849b945234e9e880822f
Asgarag <kozinandrew1@gmail.com>
2017-12-11 10:19:38 +0300

    Изменил js у кнопок в header

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,20 @@ var info1 = document.querySelector('.header__info-1');
 var info2 = document.querySelector('.header__info-2');
 var search = document.querySelector('.header__form');
 menu_switches[2].addEventListener("click", function(){
-  menu.classList.toggle('menu__list_active')
+  menu.classList.toggle('menu__list_active');
+  search.classList.remove('header__form_active');
+  info1.classList.remove('header__info_active');
+  info2.classList.remove('header__form_active');
 })
 menu_switches[1].addEventListener("click", function(){
   search.classList.toggle('header__form_active');
+  info1.classList.remove('header__info_active');
+  info2.classList.remove('header__form_active');
+  menu.classList.remove('menu__list_active');
 })
 menu_switches[0].addEventListener("click", function(){
   info1.classList.toggle('header__info_active');
   info2.classList.toggle('header__form_active');
+  search.classList.remove('header__form_active');
+  menu.classList.remove('menu__list_active');
 })
