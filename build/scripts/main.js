ab2b28e8cd7c4521c7836f6e54587d1d791d5982
Asgarag <kozinandrew1@gmail.com>
2017-12-13 01:19:15 +0300

    Изменил главную, переверстал остальные страницы

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -1,15 +1,22 @@
 var menu_switches = document.querySelectorAll('.header__switch');
 var menu = document.querySelector('.menu__list');
-var info1 = document.querySelector('.header__info-1');
-var info2 = document.querySelector('.header__info-2');
+var info = document.querySelectorAll('.header__info');
 var search = document.querySelector('.header__form');
 menu_switches[2].addEventListener("click", function(){
-  menu.classList.toggle('menu__list_active')
+  menu.classList.toggle('menu__list_active');
+  search.classList.remove('header__form_active');
+  info[0].classList.remove('header__info_active');
+  info[1].classList.remove('header__info_active');
 })
 menu_switches[1].addEventListener("click", function(){
   search.classList.toggle('header__form_active');
+  info[0].classList.remove('header__info_active');
+  info[1].classList.remove('header__info_active');
+  menu.classList.remove('menu__list_active');
 })
 menu_switches[0].addEventListener("click", function(){
-  info1.classList.toggle('header__info_active');
-  info2.classList.toggle('header__form_active');
+  info[0].classList.toggle('header__info_active');
+  info[1].classList.toggle('header__info_active');
+  search.classList.remove('header__form_active');
+  menu.classList.remove('menu__list_active');
 })
