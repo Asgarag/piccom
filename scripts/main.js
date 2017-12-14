412d43b70a15aba585f27b660d2a9ec495267d04
Asgarag <kozinandrew1@gmail.com>
2017-12-15 00:02:39 +0300

    Изменил структуру, добавил датапикер и карту

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,22 @@
 var menu_switches = document.querySelectorAll('.header__switch');
 var menu = document.querySelector('.menu__list');
-var info = document.querySelectorAll('.header__info');
 var search = document.querySelector('.header__form');
-menu_switches[2].addEventListener("click", function(){
-  menu.classList.toggle('menu__list_active');
-  search.classList.remove('header__form_active');
-  info[0].classList.remove('header__info_active');
-  info[1].classList.remove('header__info_active');
-})
 menu_switches[1].addEventListener("click", function(){
-  search.classList.toggle('header__form_active');
-  info[0].classList.remove('header__info_active');
-  info[1].classList.remove('header__info_active');
-  menu.classList.remove('menu__list_active');
+  search.classList.remove('header__form_active');
+  menu.classList.toggle('menu__list_active');
 })
 menu_switches[0].addEventListener("click", function(){
-  info[0].classList.toggle('header__info_active');
-  info[1].classList.toggle('header__info_active');
-  search.classList.remove('header__form_active');
+  search.classList.toggle('header__form_active');
   menu.classList.remove('menu__list_active');
 })
+
+var picker = new Pikaday({
+    field: document.getElementById('datepicker'),
+    format: 'D MMMM YYYY',
+    firstDay: 1,
+    minDate: new Date(2000, 0, 1),
+    maxDate: new Date(2020, 12, 31),
+    yearRange: [2000,2020],
+    showDaysInNextAndPreviousMonths: true,
+    enableSelectionDaysInNextAndPreviousMonths: true
+});
